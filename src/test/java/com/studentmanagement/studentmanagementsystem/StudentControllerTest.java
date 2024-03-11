725d79405920908729265b8f3646918e1cd8bff1
abdulwahidpanaroma <abdul.wahid@panaroma.finance>
2024-03-11 15:14:23 +0400

    Indentation changes

--- src/test/java/com/studentmanagement/studentmanagementsystem/StudentControllerTest.java
+++ src/test/java/com/studentmanagement/studentmanagementsystem/StudentControllerTest.java
@@ -43,123 +43,102 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StudentControllerTest {
 
-    @Autowired
-    MockMvc mvc;
+	@Autowired
+	MockMvc mvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
+	@Autowired
+	ObjectMapper objectMapper;
 
-    @Test
-    public void test_getAllStudents() throws Exception
-    {
-        mvc.perform(MockMvcRequestBuilders
-                        .get("/students")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
+	@Test
+	public void test_getAllStudents() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/students").accept(MediaType.APPLICATION_JSON)).andDo(print())
+				.andExpect(status().isOk());
+	}
 
-    @Test
-    public void test_getStudentById() throws Exception
-    {
-        ResultActions resultActions = mvc.perform(MockMvcRequestBuilders
-                        .get("/students/1")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk());
+	@Test
+	public void test_getStudentById() throws Exception {
+		ResultActions resultActions = mvc
+				.perform(MockMvcRequestBuilders.get("/students/1").accept(MediaType.APPLICATION_JSON)).andDo(print())
+				.andExpect(status().isOk());
 
-        Student student = convertResultActionsToStudent(resultActions);
+		Student student = convertResultActionsToStudent(resultActions);
 
-        assertAll(
-                () -> assertNotNull(student),
-                () -> assertEquals("abdul", student.getName())
-        );
-    }
+		assertAll(() -> assertNotNull(student), () -> assertEquals("abdul", student.getName()));
+	}
 
-    @Test
-    public void test_saveStudent() throws Exception {
-        Student mockStudent = new Student(1L,"abdul@gmail.com","abdul",15,LocalDate.of(2008,5,12));
-        // Performing the POST request
-        ResultActions resultActions = mvc.perform(MockMvcRequestBuilders.post("/students")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(mockStudent)))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+	@Test
+	public void test_saveStudent() throws Exception {
+		Student mockStudent = new Student(1L, "abdul@gmail.com", "abdul", 15, LocalDate.of(2008, 5, 12));
+		// Performing the POST request
+		ResultActions resultActions = mvc
+				.perform(MockMvcRequestBuilders.post("/students").contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(mockStudent)))
+				.andExpect(MockMvcResultMatchers.status().isCreated());
 
-        Student student = convertResultActionsToStudent(resultActions);
+		Student student = convertResultActionsToStudent(resultActions);
 
-        assertAll(
-                () -> assertNotNull(student),
-                () -> assertEquals(mockStudent, student),
-                () -> assertEquals(mockStudent.getName(), student.getName())
-        );
-    }
+		assertAll(() -> assertNotNull(student), () -> assertEquals(mockStudent, student),
+				() -> assertEquals(mockStudent.getName(), student.getName()));
+	}
 
-    @Test
-    public void test_createShouldNotCreateSuccessfullyForAge() throws Exception {
-        Student mockStudent = new Student(1L,"abdul@gmail.com","abdul",10,LocalDate.of(2008,5,12));
-        // Performing the POST request
-        ResultActions resultActions = mvc.perform(MockMvcRequestBuilders.post("/students")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(mockStudent)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+	@Test
+	public void test_createShouldNotCreateSuccessfullyForAge() throws Exception {
+		Student mockStudent = new Student(1L, "abdul@gmail.com", "abdul", 10, LocalDate.of(2008, 5, 12));
+		// Performing the POST request
+		ResultActions resultActions = mvc
+				.perform(MockMvcRequestBuilders.post("/students").contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(mockStudent)))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest());
 
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        assertAll(
-                () -> assertEquals("Age must be at least 15",JsonPath.read(responseBody, "$.message"))
-        );
-    }
+		String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+		assertAll(() -> assertEquals("Age must be at least 15", JsonPath.read(responseBody, "$.message")));
+	}
 
-    @Test
-    public void test_createShouldNotCreateSuccessfullyForDateOfBirth() throws Exception {
-        Student mockStudent = new Student(1L,"abdul@gmail.com","abdul",16,null);
-        // Performing the POST request
-        ResultActions resultActions = mvc.perform(MockMvcRequestBuilders.post("/students")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(mockStudent)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+	@Test
+	public void test_createShouldNotCreateSuccessfullyForDateOfBirth() throws Exception {
+		Student mockStudent = new Student(1L, "abdul@gmail.com", "abdul", 16, null);
+		// Performing the POST request
+		ResultActions resultActions = mvc
+				.perform(MockMvcRequestBuilders.post("/students").contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(mockStudent)))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest());
 
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        assertAll(
-                () -> assertEquals("Date of birth is required",JsonPath.read(responseBody, "$.message"))
-        );
-    }
+		String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+		assertAll(() -> assertEquals("Date of birth is required", JsonPath.read(responseBody, "$.message")));
+	}
 
-    @Test
-    public void test_updateStudentByEmail() throws Exception {
-        // Mocking the service response
-        Student mockStudent = new Student(1L,"abdulUpdate@gmail.com","abdul",15,LocalDate.of(2008,5,12));
+	@Test
+	public void test_updateStudentByEmail() throws Exception {
+		// Mocking the service response
+		Student mockStudent = new Student(1L, "abdulUpdate@gmail.com", "abdul", 15, LocalDate.of(2008, 5, 12));
 
-        // Creating a sample request
-        String requestBody = objectMapper.writeValueAsString(mockStudent);
+		// Creating a sample request
+		String requestBody = objectMapper.writeValueAsString(mockStudent);
 
-        // Performing the PUT request
-        ResultActions resultActions =  mvc.perform(MockMvcRequestBuilders.put("/students/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+		// Performing the PUT request
+		ResultActions resultActions = mvc.perform(
+				MockMvcRequestBuilders.put("/students/1").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 
-        Student student =convertResultActionsToStudent(resultActions);
+		Student student = convertResultActionsToStudent(resultActions);
 
-        assertAll(
-                () -> assertNotNull(student),
-                () -> assertEquals(mockStudent, student),
-                () -> assertEquals(mockStudent.getEmail(), student.getEmail())
-        );
+		assertAll(() -> assertNotNull(student), () -> assertEquals(mockStudent, student),
+				() -> assertEquals(mockStudent.getEmail(), student.getEmail()));
 
-    }
+	}
 
-    @Test
-    public void test_deleteStudent() throws Exception {
-        // Performing the DELETE request
-        mvc.perform(MockMvcRequestBuilders.delete("/students/1"))
-                .andExpect(MockMvcResultMatchers.status().isNoContent());
-    }
+	@Test
+	public void test_deleteStudent() throws Exception {
+		// Performing the DELETE request
+		mvc.perform(MockMvcRequestBuilders.delete("/students/1"))
+				.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
 
-    private Student convertResultActionsToStudent(ResultActions resultActions) throws UnsupportedEncodingException, JsonProcessingException {
-        return objectMapper.readValue(objectMapper.writeValueAsString
-                (objectMapper.readValue(resultActions.andReturn().getResponse().getContentAsString(), Response.class)
-                        .getData()), Student.class);
-    }
-
+	private Student convertResultActionsToStudent(ResultActions resultActions)
+			throws UnsupportedEncodingException, JsonProcessingException {
+		return objectMapper.readValue(objectMapper.writeValueAsString(objectMapper
+				.readValue(resultActions.andReturn().getResponse().getContentAsString(), Response.class).getData()),
+				Student.class);
+	}
 
 }
